c065425ea6afdfb554c6fb1866cd404c3c397b85
Sirine-Chi <71520044+Sirine-Chi@users.noreply.github.com>
2023-07-09 23:54:25 +0300

    Added particle class.

--- main.cpp
+++ main.cpp
@@ -6,11 +6,11 @@
 #include <fstream>
 #include <stdio.h>
 #include <stdlib.h>
+#include "string"
+#include "vector"
 #include <cmath>
 #include <ctime>
 #include "build/Eigen/Dense"
-
-#include "simulator.h"
 
 #ifdef __APPLE__
 #include <OpenCL/opencl.h>
@@ -21,11 +21,55 @@
 #define MAX_SOURCE_SIZE (0x100000)
 
 using namespace std;
-//using vector
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
 const float G = 0.0001184069; // In units Earth mass, AU, A Year
+
+// Function, gravity force acting on I, ri, rj, mi, mj
+VectorXd f_i(VectorXd& ri, VectorXd& rj, float& mi, float& mj) {
+    return (rj-ri)*G*mi*mj*pow((rj-ri).norm(), -3.0/2);
+};
+
+class Particle {
+public:
+    // Constructor with arguments
+    // Name, sPosition, sVelocity, mass,
+    Particle(string namep, float massp, VectorXd positionp, VectorXd velocityp, string colorp, float anglep) {
+        name = namep;
+        mass = massp;
+        positions.insert(positions.begin(), positionp);
+        velocities.insert(velocities.begin(), velocityp);
+        color = colorp;
+        angle = anglep;
+    };
+
+    void set_values(string& namep, float& massp, VectorXd& positionp, VectorXd& velocityp, string& colorp, float& anglep) {
+        name = namep;
+        mass = massp;
+        positions.insert(positions.begin(), positionp);
+        velocities.insert(velocities.begin(), velocityp);
+        color = colorp;
+        angle = anglep;
+    };
+
+    vector<VectorXd> get_positions() const {
+        return positions;
+    };
+private:
+    string name;
+    float mass;
+    vector<VectorXd> positions;
+    vector<VectorXd> velocities;
+    string color;
+    float angle;
+
+    // Function: gravity force acting on I, at iteration S
+    // Inputs: particle1, particle2, S
+    VectorXd f_i_s_p(Particle& p1, Particle& p2, int& s) {
+        return f_i(p1.positions[s], p2.positions[s], p1.mass, p2.mass);
+    };
+};
 
 //class Simulator {
 //    // subclasses: different iteration method
@@ -52,11 +96,6 @@ const float G = 0.0001184069; // In units Earth mass, AU, A Year
 //    vector<Particle> particles;
 //};
 
-// Return gravitational force acting on bodies with positions ri, rj, and masses mi, mj
-VectorXd f_i(VectorXd& ri, VectorXd& rj, float& mi, float& mj) {
-    return (rj-ri)*G*mi*mj*pow((ri-rj).norm(), -3.0/2);
-};
-
 int main() {
     const clock_t begin_time = clock();
 
@@ -75,8 +114,17 @@ int main() {
     VectorXd vec1 {{a, b}};
     VectorXd vec2 {{2*b, a/3}};
 
-    cout << "--- vec1\n" << vec1 << endl << "--- vec2\n" << vec2 << endl;
+    cout << "--- vec1\n" << vec1 << "\n" << "--- vec2\n" << vec2 << "\n";
     cout << "force\n" << f_i(vec1, vec2, m1, m2) << "\n";
+
+    VectorXd r {{1.016725701, 0.0}};
+    VectorXd v {{0.0, 6.1754482536}};
+    Particle earth("Earth", 1.0, r, v, "aqua", 224.0); // Consrtucting object of class
+//    Particle earth;
+//    earth.set_values("Earth", 1.0, r, v, "aqua", 224.0);
+    vector <VectorXd> poses = earth.get_positions();
+
+    for (auto & element : poses) { cout << element << "-- el \n"; }
 
     // print to output.txt
 //    ofstream out("output.txt", ios_base::out);
