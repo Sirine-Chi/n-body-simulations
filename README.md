72abee9d7b815f7f4c8e60acf16f94d9e8304028
Sirine-Chi <71520044+Sirine-Chi@users.noreply.github.com>
2023-07-09 23:31:46 +0300

    Oh

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # n-body-simulations
 
 Version on C++, with Eigen as library for vector math computations.
+
+
+## Installation
+
+### Source
+Clone with git, or download zip, as you want. You can run builded executable, or, build by yourself. But for this make sure that you have c++ compiler and IDE.
+
+### Libraries
+Make a folder "build" in our main direcrory, parallel with main.
+
+From [official website](https://eigen.tuxfamily.org/index.php?title=Main_Page) download Eigen (Library for vector math computations), and place the "Eigen" folder into the build folder, it's ready!
+From [yaml-cpp github](https://github.com/jbeder/yaml-cpp) download source, find `yaml-cpp/include/yaml-cpp` folder, and place deeper `yaml-cpp` to the build folder.
+
+## Copyright
+
+MIT License, open source.
--- main.cpp
+++ main.cpp
@@ -7,7 +7,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <cmath>
-#include "../eigen/Eigen/Dense"
+#include <ctime>
+#include "build/Eigen/Dense"
+
+#include "simulator.h"
 
 #ifdef __APPLE__
 #include <OpenCL/opencl.h>
@@ -18,38 +21,53 @@
 #define MAX_SOURCE_SIZE (0x100000)
 
 using namespace std;
+//using vector
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
 const float G = 0.0001184069; // In units Earth mass, AU, A Year
 
-class Simulator {
-public:
-    // Constant
-    float G = 0.0001184069;
-};
+//class Simulator {
+//    // subclasses: different iteration method
+//
+//    // def Initialise: set variables and vectors
+//    // def Iterator method: add new components to RS, VS, AS
+//    // def Render method: specify start time, in loop Sim.iteration(self), save delta
+//    // def get_time
+//    // def get_positions
+//    // def get_velocities
+//public:
+//    // Constant
+//    float G = 0.0001184069;
+//
+//    void set_objects(vector<Particle>) {
+//
+//    };
+//
+//    vector<VectorXd> get_positions() {
+//        return
+//    };
+//
+//private:
+//    vector<Particle> particles;
+//};
 
 // Return gravitational force acting on bodies with positions ri, rj, and masses mi, mj
-VectorXd f_i(VectorXd ri, VectorXd rj, float mi, float mj) {
+VectorXd f_i(VectorXd& ri, VectorXd& rj, float& mi, float& mj) {
     return (rj-ri)*G*mi*mj*pow((ri-rj).norm(), -3.0/2);
 };
 
-struct Particle {
-    string name;
-    vector <VectorXd> rs; // Не уверен, может нужны вектора векторов или матрицы?
-    vector <VectorXd> vs;
-    float m;
-    string color;
-};
-
 int main() {
-    char was[50];
-    ifstream in("input.txt", ios_base::in);
-    in.getline(was, 50);
-    in.close();
+    const clock_t begin_time = clock();
 
-    cout << was;
+    // read text
+//    char was[50];
+//    ifstream in("input.txt", ios_base::in);
+//    in.getline(was, 50);
+//    in.close();
+//    cout << was;
 
+    // set values
     float a = 1.2;
     float b = 3.2;
     float m1 = 1.3;
@@ -58,12 +76,15 @@ int main() {
     VectorXd vec2 {{2*b, a/3}};
 
     cout << "--- vec1\n" << vec1 << endl << "--- vec2\n" << vec2 << endl;
-    cout << "force\n" << f_i(vec1, vec2, m1, m2);
+    cout << "force\n" << f_i(vec1, vec2, m1, m2) << "\n";
 
-    ofstream out("output.txt", ios_base::out);
-    out << "--- vec1\n" << vec1 << endl << "--- vec2\n" << vec2 << endl;
-    out << "force\n" << f_i(vec1, vec2, m1, m2);
-    out.close();
+    // print to output.txt
+//    ofstream out("output.txt", ios_base::out);
+//    out << "--- vec1\n" << vec1 << endl << "--- vec2\n" << vec2 << endl;
+//    out << "force\n" << f_i(vec1, vec2, m1, m2) << "\n";
+//    out.close();
 
+    // print runtime
+    cout << float( clock() - begin_time ) /  CLOCKS_PER_SEC << " sec" << endl;
     return 0;
 }
